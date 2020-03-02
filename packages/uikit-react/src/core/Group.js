e3a4bb550557b5de8f1f003410cfd1e55a6a2cca
Mikhail Shcherbinin <mu5h3r@30l.io>
2020-03-02 13:42:51 +0300

    Group style prop

--- packages/uikit-react/src/core/Group.js
+++ packages/uikit-react/src/core/Group.js
@@ -2,5 +2,5 @@ import React from 'react';
 import '@mu5h3r/uikit/core/group.scss';
 
 export default function Group(props) {
-  return <div className={props.className ? `group ${props.className}` : 'group'}>{props.children}</div>;
+  return <div className={props.className ? `group ${props.className}` : 'group'} style={props.style}>{props.children}</div>;
 }
