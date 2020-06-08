07b6c547b1f2cabc91f3a3c2a882a5b823e5f4de
Михаил Щербинин <mu5h3r@30l.io>
2020-06-08 18:05:51 +0300

    Publish
     - @mu5h3r/uikit-react@0.111.89
     - @mu5h3r/uikit@0.1.88

--- packages/uikit-react/src/form/input/Text.js
+++ packages/uikit-react/src/form/input/Text.js
@@ -7,41 +7,36 @@ import Label from '../../core/Label';
 import '@mu5h3r/uikit/form/input/text.scss';
 
 type Props = {
+    autoFocus ?: boolean,
     type ?: string,
+    className?: string,
     onClick?: () => void,
-    onChange?: () => void,
-    className?: string
+    onChange?: () => void
 };
 
-export default class Text extends React.Component<Props> {
-    state = {
-        focus: false,
-        empty: true
-    };
+export default function (props: Props) {
+    const { type, autoFocus, className, style, help, label, value, readOnly, suffix, prefix, error, onChange, onClick } = props;
 
-    render() {
-        const { label, value, readOnly, suffix, prefix, error, onChange, onClick, type, className, style, help } = this.props;
+    let classes = 'uikit-input-text';
+    if (!value) classes += ' empty';
+    if (suffix || prefix) classes += ' inline';
+    if (className) classes += ` ${className}`;
+    if (error) classes += ' error';
 
-        let classes = 'uikit-input-text';
-        if (!value) classes += ' empty';
-        if (suffix || prefix) classes += ' inline';
-        if (className) classes += ` ${className}`;
-        if (error) classes += ' error';
-
-        return <Group style={style} className={`uikit-input-wrapper`}>
-            <Base className={classes} onClick={onClick}>
-                { prefix ? <div className="uikit-input__prefix">{prefix}</div> : null }
-                <input type={type || 'text'}
-                       readOnly={readOnly}
-                       value={value || ''}
-                       onChange={(e) => onChange(e.currentTarget.value)} />
-                { this.props.children }
-                { suffix ? <div className="uikit-input__suffix">{suffix}</div> : null }
-                { error ? <div className="uikit-input__suffix"><Icon>error</Icon></div>: null }
-                { label ? <Label>{label}</Label> : null }
-            </Base>
-            { help ? <div className="help-text">{help}</div> : null }
-            { error ? <div className="error-text">{error}</div> : null }
-        </Group>
-    }
+    return <Group style={style} className={`uikit-input-wrapper`}>
+        <Base className={classes} onClick={onClick}>
+            { prefix ? <div className="uikit-input__prefix">{prefix}</div> : null }
+            <input type={type || 'text'}
+                   autoFocus={autoFocus || false}
+                   readOnly={readOnly}
+                   value={value || ''}
+                   onChange={(e) => onChange(e.currentTarget.value)} />
+            { props.children }
+            { suffix ? <div className="uikit-input__suffix">{suffix}</div> : null }
+            { error ? <div className="uikit-input__suffix"><Icon>error</Icon></div>: null }
+            { label ? <Label>{label}</Label> : null }
+        </Base>
+        { help ? <div className="help-text">{help}</div> : null }
+        { error ? <div className="error-text">{error}</div> : null }
+    </Group>
 }
